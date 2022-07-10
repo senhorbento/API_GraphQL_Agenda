e9976228efb99760c0f13dab681eeb216db2719b
senhorbento <sbento.ti@gmail.com>
2022-07-10 11:49:22 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { ApolloServer, gql} = require("apollo-server");
+
+const typeDefs = gql`
+    type Query{
+        hello : String
+    }
+`;
+const resolvers = {
+    Query:{
+        hello : () => "Hello World!"
+    }
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen(3001);
