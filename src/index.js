b1dcdcdfa9ec1a5f9d6803a3e4ddf1aa3d0cd62d
senhorbento <sbento.ti@gmail.com>
2022-07-11 11:28:47 -0300

    Feat: Add prototipos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
+const PORTA = 3001
 const { ApolloServer, gql} = require("apollo-server");
 
 const typeDefs = gql`
+    type Contato{
+        _id: ID
+        nome: String!
+        telefone: String!
+        email: String
+    }
     type Query{
-        hello : String
+        agenda: [Contato!]!
+        contato: Contato!
+        obterContatoPorEmail(email: String!): Contato!
+        obterContatoPorTelefone(telefone: String!): Contato!
+    }
+    type Mutation{
+        criarContato(nome: String!, telefone: String!, email: String): Contato!
     }
 `;
 const resolvers = {
     Query:{
-        hello : () => "Hello World!"
+        agenda: () => "Hello World!",
+        contato: () => "Hello World!",
+        obterContatoPorEmail: (_,args) =>"args.email",
+        obterContatoPorTelefone: (_,args) => "args.telefone"
+    },
+    Mutation:{
+        criarContato: (_,args) => {
+            const novoContato = {
+                nome: args.nome,
+                telefone: args.telefone,
+                email: args.email
+            }
+            return novoContato;
+        }
     }
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
 
-server.listen(3001);
+server.listen(PORTA);
